5cd727cf11b62ad12f6fce21433c56da73985184
Seong il Lee <118361080+Sun6004@users.noreply.github.com>
2023-02-13 21:31:23 +0900

    Add files via upload

--- basicProject/FrontController.java
+++ basicProject/FrontController.java
@@ -10,7 +10,7 @@ public class FrontController {
 		while(true) {
 			System.out.println("|  			  Hair Shop 	    	 |");
 			System.out.println("------------------------------------");
-			System.out.println("1.회원로그인 2.관리자로그인 3.회원가입 4종료 ");
+			System.out.println("1.회원로그인 2.관리자로그인 3.회원가입 0.종료 ");
 			System.out.println("------------------------------------");
 			System.out.println("메뉴선택: ");
 			int selectMenu = Integer.parseInt(scanner.nextLine());
@@ -23,15 +23,37 @@ public class FrontController {
 				
 				break;
 			case 3:
+				String regEx = "(\\d{3})(\\d{4})(\\d{4})";
 				
+				System.out.print("ID를 입력해주세요> ");
+				String newId = scanner.nextLine();
+				System.out.print("비밀번호를 입력해주세요> ");
+				String newPw = scanner.nextLine();
+				System.out.print("비밀번호 확인> ");
+				String newPw2 = scanner.nextLine();
+				
+				boolean success = false;					
+				if (newPw.equals(newPw2)) {
+					success = true;
+				}if(success) {
+					System.out.println("비밀번호가 일치합니다.");
+				}else {
+					System.out.println("비밀번호가 일치하지 않습니다.");
+				}
+				System.out.print("이름을 입력해주세요> ");
+				String newName = scanner.nextLine();
+				System.out.print("생년월일을 입력해주세요(yyyyddmm방식)");
+				String newBir = scanner.nextLine();
+				System.out.print("전화번호를 입력해주세요> ");
+				String newPh = scanner.nextLine().replaceAll(regEx, "$1-$2-$3");
+				ReservationController.signUpMember(new MemberVO(newId, newPw, newName, newBir, newPh));
 				break;
-			case 4:
+			case 0:
 				System.out.println("프로그램을 종료합니다.");
 				System.exit(0);
 				break;
 			}
-			
-			
+
 		}
 	}
 }
--- basicProject/ReservationController.java
+++ basicProject/ReservationController.java
@@ -2,4 +2,7 @@ package basicProject;
 
 public class ReservationController {
 
+	public static void signUpMember(MemberVO memberVO) {
+	}
+
 }
